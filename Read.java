611c416462b8ec422735fa37cad0104f48c31b1b
Pranav P <153451679+ptowncs@users.noreply.github.com>
2025-10-10 16:55:06 -0700

    Create Read.java

---
+++ Read.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
+public class Read {
+    public void readData(){
+        try {
+            Scanner in = new Scanner(new File("songs 2025-2026 ANSI.csv"));
+            while(in.hasNext()){
+                System.out.print(in.nextLine());
+            }
+        }catch(IOException e){
+            System.out.println("File not found");
+        }
+    }
+}
