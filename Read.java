206cb1a5147d8d562a2246864c0934f6eb4622f3
Pranav P <153451679+ptowncs@users.noreply.github.com>
2025-10-11 00:17:32 +0000

    add file

--- Read.java
+++ Read.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Read {
     public void readData(){
         try {
-            Scanner in = new Scanner(new File("songs 2025-2026 ANSI.csv"));
+            Scanner in = new Scanner(new File("songs 2025-2026.csv"));
             while(in.hasNext()){
                 System.out.print(in.nextLine());
             }
